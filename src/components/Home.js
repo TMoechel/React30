7dc7df543244565fc3317959e31aa50428d33585
t.moechel <t.moechel@softcreation.de>
2019-03-12 08:04:33 +0100

    react-redux

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
+import {fetchNewTime} from '../redux/actionCreators';
 
 
 const Home = (props) => {
@@ -8,17 +9,24 @@ const Home = (props) => {
         <div>
             <h1>Welcome home</h1>
             <p>Current time: {props.currentTime}</p>
+            <button onClick={props.updateTime}>Update Time</button>
             <Link to='/about'>Go to
                about</Link>
         </div>
     )
 }
-
 const mapStateToProps = state => {
+    //assigns the state.currentTime to the currentTime prop of Home component
     return {
         currentTime: state.currentTime
     }
 }
 
+const mapDispatchToProps = (dispatch) => {
+    return  {
+        updateTime: () => dispatch(fetchNewTime())
+    }
+}
 
-export default connect(mapStateToProps)(Home);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
